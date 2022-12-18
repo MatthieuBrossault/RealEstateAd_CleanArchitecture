e13d48bfd96ca382048f8b9c08dafd2840ad78b1
Matthieu BROSSAULT <ext.matthieu.brossault@bollore.com>
2022-12-18 21:29:14 +0100

    Add RealEstateController

---
+++ src/Application/RealEstate/Commands/UpdateRealEstateAdCommand.cs
@@ -0,0 +1,41 @@
+﻿using RealEstateAd_CleanArchitecture.Application.Common.Exceptions;
+using RealEstateAd_CleanArchitecture.Application.Common.Interfaces;
+using RealEstateAd_CleanArchitecture.Domain.Entities.RealEstate;
+using RealEstateAd_CleanArchitecture.Domain.Enums;
+using MediatR;
+
+namespace RealEstateAd_CleanArchitecture.Application.RealEstate.Commands.UpdateRealEstateAd;
+
+public record UpdateRealEstateAdCommand : IRequest
+{
+    public int Id { get; init; }
+
+    public int PublicationStatus { get; init; }
+}
+
+public class UpdateRealEstateAdCommandHandler : IRequestHandler<UpdateRealEstateAdCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public UpdateRealEstateAdCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(UpdateRealEstateAdCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.RealEstateAds
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
+        if (entity == null)
+        {
+            throw new NotFoundException(nameof(RealEstateAd), request.Id);
+        }
+
+        entity.PublicationStatus = (PublicationStatus)request.PublicationStatus;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
---
+++ src/WebUI/Controllers/RealEstateAdController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateAd_CleanArchitecture.Application.RealEstate.Queries;
+using RealEstateAd_CleanArchitecture.Application.RealEstate.Queries.GetRealEstateAdById;
+using RealEstateAd_CleanArchitecture.Application.RealEstate.Commands.CreateRealEstateAd;
+using RealEstateAd_CleanArchitecture.Application.RealEstate.Commands.UpdateRealEstateAd;
+
+namespace RealEstateAd_CleanArchitecture.WebUI.Controllers;
+
+[Authorize]
+public class RealEstateAdController : ApiControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<RealEstateAdDto>> GetRealEstateAdById([FromQuery] GetRealEstateAdByIdQuery query)
+    {
+        return await Mediator.Send(query);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> Create(CreateRealEstateAdCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Update(int id, UpdateRealEstateAdCommand command)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest();
+        }
+
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+}
